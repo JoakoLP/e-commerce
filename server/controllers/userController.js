d0a8234b2a8ee890dc499e040586e396c3ebc531
JoakoLP <joakotakara@gmail.com>
2023-10-04 10:36:10 -0300

    cookie

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -41,7 +41,7 @@ class UserController {
         res.cookie(
           "userSession",
           { ...req.session.user },
-          req.body.remember ? {} : { maxAge: 60000 }
+          req.body.remember ? { sameSite: "none" } : { maxAge: 60000, sameSite: "none" }
           // req.body.remember ? { domain: "https://e-commerce-five-rose.vercel.app/" } : { maxAge: 60000, domain: "https://e-commerce-five-rose.vercel.app/" }
           // req.body.remember ? { domain: "https://e-commerce-server-psi.vercel.app/" } : { maxAge: 60000, domain: "https://e-commerce-server-psi.vercel.app/" }
         );
