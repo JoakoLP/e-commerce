3000d1efcb45f24e9a9f8829f441acf752aebdf9
JoakoLP <joakotakara@gmail.com>
2023-10-04 15:56:01 -0300

    testing

--- server/app.js
+++ server/app.js
@@ -36,7 +36,8 @@ app.use(
 app.use(cookiesMiddleware());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.APP_URL);
+  res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Origin", process.env.APP_URL);
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
