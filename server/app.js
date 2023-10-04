aa97c04adcb057b3bb62c68bb12942e8d5da1d0f
JoakoLP <joakotakara@gmail.com>
2023-10-04 16:52:15 -0300

    headers

--- server/app.js
+++ server/app.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
   // res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Origin", process.env.APP_URL);
   // res.header("Access-Control-Allow-Origin", "https://e-commerce.joaquintakara.com");
-  res.header("Access-Control-Allow-Methods", "POST, GET, PUT");
+  // res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Credentials", true);
   next();
