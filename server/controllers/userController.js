94a29a6185c0e3ccb7d40b737ecebe71a8b8c9dc
JoakoLP <joakotakara@gmail.com>
2023-10-04 10:22:17 -0300

    fix

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -34,7 +34,7 @@ class UserController {
           "authorization",
           token,
           // `Bearer ${token}`,
-          req.body.remember ? {} : { maxAge: 60000 }
+          // req.body.remember ? {} : { maxAge: 60000 }
           req.body.remember ? { domain: "e-commerce-five-rose.vercel.app" } : { maxAge: 60000, domain: "e-commerce-five-rose.vercel.app" }
           // req.body.remember ? { domain: "https://e-commerce-server-psi.vercel.app/" } : { maxAge: 60000, domain: "https://e-commerce-server-psi.vercel.app/" }
         );
