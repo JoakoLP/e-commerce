f1d96bd5dd3d126f8aebf1ab672ef4481eee6f12
JoakoLP <joakotakara@gmail.com>
2023-10-04 10:40:15 -0300

    cookie

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -35,13 +35,15 @@ class UserController {
           token,
           // `Bearer ${token}`,
           // req.body.remember ? {} : { maxAge: 60000 }
-          req.body.remember ? { domain: "e-commerce-five-rose.vercel.app" } : { maxAge: 60000, domain: "e-commerce-five-rose.vercel.app", sameSite: "none" }
+          req.body.remember
+            ? { domain: "e-commerce-five-rose.vercel.app", sameSite: "none", secure: true }
+            : { maxAge: 60000, domain: "e-commerce-five-rose.vercel.app", sameSite: "none", secure: true }
           // req.body.remember ? { domain: "https://e-commerce-server-psi.vercel.app/" } : { maxAge: 60000, domain: "https://e-commerce-server-psi.vercel.app/" }
         );
         res.cookie(
           "userSession",
           { ...req.session.user },
-          req.body.remember ? { sameSite: "none" } : { maxAge: 60000, sameSite: "none" }
+          req.body.remember ? { sameSite: "none", secure: true } : { maxAge: 60000, sameSite: "none", secure: true }
           // req.body.remember ? { domain: "https://e-commerce-five-rose.vercel.app/" } : { maxAge: 60000, domain: "https://e-commerce-five-rose.vercel.app/" }
           // req.body.remember ? { domain: "https://e-commerce-server-psi.vercel.app/" } : { maxAge: 60000, domain: "https://e-commerce-server-psi.vercel.app/" }
         );
