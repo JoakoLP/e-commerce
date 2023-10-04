3419d521d871beb14b4b7ef31092dc9ef5d81a93
JoakoLP <joakotakara@gmail.com>
2023-10-04 16:51:48 -0300

    headers

--- server/app.js
+++ server/app.js
@@ -38,8 +38,10 @@ app.use(cookiesMiddleware());
 app.use((req, res, next) => {
   // res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Origin", process.env.APP_URL);
-  res.header("Access-Control-Allow-Credentials", true);
+  // res.header("Access-Control-Allow-Origin", "https://e-commerce.joaquintakara.com");
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials", true);
   next();
 });
 
