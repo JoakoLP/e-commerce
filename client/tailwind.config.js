af198dae0cdb5ad6b1e763bd2e76461e3ce9586b
JoakoLP <joakotakara@gmail.com>
2023-10-05 10:10:58 -0300

    material tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const withMT = require("@material-tailwind/react/utils/withMT");
+const colors = require("tailwindcss/colors");
 
-module.exports = {
+module.exports = withMT({
   content: ["./src/**/*.{html,js,jsx}", "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
         "custom-blue": "#455eeb",
+        ...colors,
       },
     },
   },
-  plugins: [require("flowbite/plugin")],
-};
+  plugins: [require("tailwindcss"), require("flowbite/plugin")],
+});
