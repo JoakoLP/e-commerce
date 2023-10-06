1bc76fdf7e65c705a9ac05ee780d111ae1c27747
JoakoLP <joakotakara@gmail.com>
2023-10-06 19:59:35 -0300

    display button addToCart only when user is logged

--- client/src/components/main/Products/Product.js
+++ client/src/components/main/Products/Product.js
@@ -1,10 +1,12 @@
 import React, { useContext } from "react";
 import { CartContext } from "../../../contexts/CartProvider";
+import { AccountContext } from "../../../contexts/AccountProvider";
 
 const Product = (props) => {
   const { data } = props;
 
   // useContext;
+  const [user, setUser] = useContext(AccountContext);
   const cartContextValue = useContext(CartContext);
   const actions = cartContextValue.cartService;
   const { cart, setCart } = cartContextValue;
@@ -22,16 +24,18 @@ const Product = (props) => {
         <p className="text-xs sm:text-sm md:text-base text-left sm:text-center h-[64px] sm:h-[80px] md:h-[96px] text-ellipsis text-black overflow-hidden hover:overflow-visible">{data.name}</p>
         <div className="flex flex-col self-end sm:w-full sm:self-auto w-min">
           <span className="py-1 font-bold text-center text-black rounded-full ">${data.price}</span>
-          <button
-            onClick={() => {
-              actions.addToCart(data.prod_id, setCart, data.name);
+          {user ? (
+            <button
+              onClick={() => {
+                actions.addToCart(data.prod_id, setCart, data.name);
 
-              // cartService.addToCart(data.id);
-            }}
-            className="self-center px-2 py-0.5 text-sm font-bold text-white duration-100 rounded select-none bg-cyan-700 sm:py-2 sm:px-4 active:scale-110 active:duration-75 lg:hover:bg-cyan-900 lg:hover:shadow-inner lg:hover:shadow-neutral-800 active:shadow-inner active:shadow-neutral-800 w-min whitespace-nowrap"
-          >
-            Agregar al Carrito
-          </button>
+                // cartService.addToCart(data.id);
+              }}
+              className="self-center px-2 py-0.5 text-sm font-bold text-white duration-100 rounded select-none bg-cyan-700 sm:py-2 sm:px-4 active:scale-110 active:duration-75 lg:hover:bg-cyan-900 lg:hover:shadow-inner lg:hover:shadow-neutral-800 active:shadow-inner active:shadow-neutral-800 w-min whitespace-nowrap"
+            >
+              Agregar al Carrito
+            </button>
+          ) : null}
         </div>
       </div>
     </div>
