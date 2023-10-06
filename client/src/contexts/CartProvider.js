3d40243a33f009b038df716d3b7fe008f6441bd2
JoakoLP <joakotakara@gmail.com>
2023-10-05 21:31:09 -0300

    not asking fot cart when there's no session

--- client/src/contexts/CartProvider.js
+++ client/src/contexts/CartProvider.js
@@ -2,16 +2,20 @@ import { createContext, useEffect, useReducer, useState } from "react";
 import productsService from "../services/products";
 
 import cartService from "../services/cart";
+import Cookies from "universal-cookie";
 
 export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
+  const cookies = new Cookies();
   const [productList, setProductList] = useState();
   const [cart, setCart] = useState();
 
   useEffect(() => {
     productsService.productGet(setProductList);
-    cartService.getCart(setCart);
+    if (cookies.get("userSession")) {
+      cartService.getCart(setCart);
+    }
   }, []);
 
   const cartContextValue = { productList, setProductList, cartService, cart, setCart };
